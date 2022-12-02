9f68c1e9a19a00c79305e4c28b4ac7ff416e013c
bazinm <morgan.bazin@etu.univ-smb.fr>
2022-12-02 01:34:53 +0100

    getGame

---
+++ src/route/game/get.ts
@@ -0,0 +1,48 @@
+import { Response, Request } from "express";
+import { DB } from "../../utils/db";
+import { setHeader } from "../../utils/header";
+
+
+export const getGame = (req: Request, res: Response) => {
+    setHeader(req, res);
+    const id: any = req.query.id;
+    DB.query(`
+            SELECT
+                Game.id AS game_id,
+                Game.title AS game_title,
+                Game.description AS game_description,
+
+                Question.id AS question_id,
+                Question.title AS question_title,
+                Question.title AS question_title,
+                Question.picture AS question_picture,
+                Question.timer AS question_timer,
+                Question.description AS question_description,
+                Question.good_answer AS question_good_answer
+
+            FROM Game
+
+            INNER JOIN Question
+            ON Question.id_game = Game.id
+
+            WHERE Game.id = ?
+            `,
+        [parseInt(id)],
+        function (err, game) {
+            if (err) throw err;
+
+            if (game.length >= 0) {
+                res.send({
+                    "result": game
+                });
+            }
+            else {
+                res.send({
+                    error: "not_found"
+                });
+            }
+        }
+    );
+}
+
+
--- src/route/game/index.ts
+++ src/route/game/index.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import { listGame } from './list';
+import { getGame } from './get';
 
 const BASE_URL = '/game'
 
 export const gameRoute = (app: Express) => {
+    app.get(BASE_URL + "/get", getGame);
     app.get(BASE_URL + "/list", listGame);
 }
